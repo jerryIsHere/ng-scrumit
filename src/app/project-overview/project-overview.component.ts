9fdfaec6345257d3fd3819c04e9a59b0bed63920
jerry <14075364d@connect.polyu.hk>
2020-10-09 21:21:19 +0800

    fix mistake about formGroup & formControl

--- src/app/project-overview/project-overview.component.ts
+++ src/app/project-overview/project-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ApiAgentService } from './../api-agent.service';
 
@@ -13,9 +14,16 @@ export class ProjectOverviewComponent implements OnInit {
   constructor(public api: ApiAgentService, public route: ActivatedRoute) {
 
   }
-
+  projectForm: FormGroup = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl(''),
+    description: new FormControl(''),
+    creation: new FormControl(''),
+  }
+  )
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
+      console.log(Number(params.get('id')))
       this.api.getProject(Number(params.get('id'))).then(data => {
 
       })
