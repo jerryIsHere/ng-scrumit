f88d73c87b1cc01af11309c2ec40eb41d204b16f
jerry <14075364d@connect.polyu.hk>
2020-10-26 18:20:44 +0800

    rename transform parameter name

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -6,20 +6,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(objs: Array<unknown>, key, values?, map?): unknown {
+  transform(objs: Array<unknown>, key, values?, targetNestedArray?): unknown {
 
     if (!values) return objs;
     if (typeof key == 'string') {
       if (Array.isArray(values)) {
         if (values.length == 0) return objs
         return objs.filter(o => {
-          if (map) return o[map].filter(e => values.includes(e[key as string])).length > 0
+          if (targetNestedArray) return o[targetNestedArray].filter(e => values.includes(e[key as string])).length > 0
           return values.includes(o[key as string])
         })
       }
       else {
         return objs.filter(o => {
-          if (map) return o[map].filter(e => e[key as string] == values).length > 0
+          if (targetNestedArray) return o[targetNestedArray].filter(e => e[key as string] == values).length > 0
           return o[key as string] == values
         })
       }
@@ -30,13 +30,13 @@ export class FilterPipe implements PipeTransform {
       for (let k of key) {
         if (Array.isArray(values)) {
           result.push(...objs.filter(o => {
-            if (map) return o[map].filter(e => values.includes(e[k])).length > 0
+            if (targetNestedArray) return o[targetNestedArray].filter(e => values.includes(e[k])).length > 0
             return values.includes(o[k])
           }))
         }
         else {
           result.push(...objs.filter(o => {
-            if (map) return o[map].filter(e => e[k] == values).length > 0
+            if (targetNestedArray) return o[targetNestedArray].filter(e => e[k] == values).length > 0
             return o[k] == values
           }))
         }
